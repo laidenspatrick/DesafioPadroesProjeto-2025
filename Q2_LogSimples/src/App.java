e22371bafb4be65da970bd86152330f69f47cd9d
laidenspatrick <laidenspatrick@gmail.com>
2025-08-26 21:05:04 +0000

    Exercicio 2

--- Q2_LogSimples/src/App.java
+++ Q2_LogSimples/src/App.java
@@ -1,12 +1,20 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        LogSimples log = new LogSimples();
-        log.log("Mensagem de alerta 1");
-        log.log("Mensagem de alarme 23");
-        log.log("Mensagem de PANICO!!");
+    public static void main(String[] args) {
+        LogSimples log1 = LogSimples.getInstancia();
+        log1.log("Sistema iniciado");
 
-        for(String m:log){
-            System.out.println(m);
+        LogSimples log2 = LogSimples.getInstancia();
+        log2.log("Usuário logado");
+
+        LogSimples log3 = LogSimples.getInstancia();
+        log3.log("Operação concluída");
+
+        System.out.println("Mensagens do Log:");
+        for (String msg : log1) {
+            System.out.println(msg);
         }
+
+        System.out.println("\nlog1 == log2 ? " + (log1 == log2)); // true
+        System.out.println("log2 == log3 ? " + (log2 == log3)); // true
     }
 }
--- Q2_LogSimples/src/LogSimples.java
+++ Q2_LogSimples/src/LogSimples.java
@@ -3,14 +3,22 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-public class LogSimples implements Iterable<String>{
+public class LogSimples implements Iterable<String> {
+    private static LogSimples instancia;
     private List<String> mensagens;
 
-    public LogSimples(){
+    private LogSimples() {
         mensagens = new LinkedList<>();
     }
 
-    public void log(String m){
+    public static synchronized LogSimples getInstancia() {
+        if (instancia == null) {
+            instancia = new LogSimples();
+        }
+        return instancia;
+    }
+
+    public void log(String m) {
         String logM = LocalDate.now().toString() + " : " + m;
         mensagens.add(logM);
     }
